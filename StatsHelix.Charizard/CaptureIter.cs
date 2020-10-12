9156345a278c99ced6e0a48acbeadeb08b65028f
main() <main@ehvag.de>
2020-10-12 20:40:12 +0200

    Implement Range header parsing

---
+++ StatsHelix.Charizard/CaptureIter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StatsHelix.Charizard
+{
+    public static class CaptureIter
+    {
+        public static IEnumerable<string> ToEnumerable(this CaptureCollection c) => Enumerable.Range(0, c.Count).Select(i => c[i].Value);
+    }
+}
